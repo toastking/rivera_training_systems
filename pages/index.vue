477fb6b8a14f9f25836a3d14d56266adaf0b6b79
Matt Del Signore <mattdelsig@me.com>
2020-08-18 21:46:23 -0400

    adds identity script to the index page

--- pages/index.vue
+++ pages/index.vue
@@ -8,5 +8,12 @@ import Hero from '~/components/Hero.vue'
 export default {
   name: 'HomePage',
   components: { Hero },
+  head() {
+    return {
+      script: [
+        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' },
+      ],
+    }
+  },
 }
 </script>
