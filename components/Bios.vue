a6f1b64f9c3b01d0029071f96a7e707670708af3
Matt Del Signore <mattdelsig@me.com>
2020-08-19 23:00:41 -0400

    Added bios

---
+++ components/Bios.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="section has-background-white">
+    <div class="columns">
+      <div class="column is-full">
+        <h2
+          class="is-uppercase is-italic has-text-centered has-text-primary title is-2"
+        >
+          Who We Are
+        </h2>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column" v-for="bio of bios" :key="bio.name">
+        <figure class="image image is-128x128">
+          <img :src="bio.headshot" />
+        </figure>
+        <h3 class="title is-3">
+          <span class="has-text-weight-semibold is-uppercase">
+            {{ bio.name }}
+          </span>
+          <span class="has-text-weight-light is-italic">
+            {{ bio.credentials }}
+          </span>
+        </h3>
+        <p>
+          {{ bio.bio }}
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+
+/** Fields needed to show the biographical info on a trainer */
+interface Bio {
+  name: string;
+  credentials: string;
+  bio: string;
+  headshot: string;
+}
+
+export default Vue.extend({
+  props: {
+    bios: {
+      type: Array as PropType<Bio[]>,
+      required: true,
+    },
+  },
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -3,28 +3,29 @@
     <hero />
     <how-we-work />
     <services :services="landingPage.services" />
+    <bios :bios="landingPage.bios" />
   </div>
 </template>
 
 <script lang="ts">
-import Hero from '~/components/Hero.vue'
-import HowWeWork from '~/components/HowWeWork.vue'
-import Services from '~/components/Services.vue'
-import { Context } from '@nuxt/types'
+import Hero from '~/components/Hero.vue';
+import HowWeWork from '~/components/HowWeWork.vue';
+import Services from '~/components/Services.vue';
+import { Context } from '@nuxt/types';
 
 export default {
   name: 'HomePage',
   components: { Hero, HowWeWork, Services },
   async asyncData(context: Context) {
-    const landingPage = await context.$content('pages', 'landingpage').fetch()
-    return { landingPage }
+    const landingPage = await context.$content('pages', 'landingpage').fetch();
+    return { landingPage };
   },
   head() {
     return {
       script: [
         { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' },
       ],
-    }
+    };
   },
-}
+};
 </script>
