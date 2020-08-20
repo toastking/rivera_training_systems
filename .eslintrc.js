90481c7940c51e20b7ebfdae87b86449dc862553
Matt Del Signore <mattdelsig@me.com>
2020-08-19 21:44:19 -0400

    Updated Eslint/prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,18 +9,18 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/recommended',
-    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
   ],
   // required to lint *.vue files
   plugins: ['vue'],
   // add your custom rules here
   rules: {
-    semi: [2, 'never'],
     'no-console': 'off',
     'vue/max-attributes-per-line': 'off',
-    'prettier/prettier': ['error', { semi: false }],
   },
-}
+};
