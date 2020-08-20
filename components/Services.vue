c96a724098260ee650a7a35576d04b11ab783e85
Matt Del Signore <mattdelsig@me.com>
2020-08-19 21:09:17 -0400

    Added What We Do section

---
+++ components/Services.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="section has-background-blue-grey">
+    <div class="columns">
+      <div class="column is-full">
+        <h2
+          class="title is-2 has-text-white is-uppercase has-text-centered has-text-weight-semibold"
+        >
+          What We Do
+        </h2>
+      </div>
+    </div>
+
+    <div class="columns">
+      <div v-for="service of services" :key="service" class="column">
+        <p class="has-text-white has-text-centered is-uppercase is-size-4">
+          {{ service }}
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+
+export default Vue.extend({
+  props: {
+    services: {
+      type: Array as PropType<string[]>,
+      required: true,
+    },
+  },
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,23 @@
   <div>
     <hero />
     <how-we-work />
+    <services :services="landingPage.services" />
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import Hero from '~/components/Hero.vue'
 import HowWeWork from '~/components/HowWeWork.vue'
+import Services from '~/components/Services.vue'
+import { Context } from '@nuxt/types'
 
 export default {
   name: 'HomePage',
-  components: { Hero, HowWeWork },
+  components: { Hero, HowWeWork, Services },
+  async asyncData(context: Context) {
+    const landingPage = await context.$content('pages', 'landingpage').fetch()
+    return { landingPage }
+  },
   head() {
     return {
       script: [
