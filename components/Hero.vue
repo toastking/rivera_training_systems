2b76e61841e13a897dd83500fa9455eedac1c5a2
Matt Del Signore <mattdelsig@me.com>
2020-08-20 23:46:06 -0400

    Hooked up the email form to netlify forms

--- components/Hero.vue
+++ components/Hero.vue
@@ -2,17 +2,38 @@
   <section class="hero is-medium is-dark">
     <div class="hero-body">
       <div class="container is-fullhd">
-        <h1 class="title">
+        <h1 class="title is-2">
           Reach your goals
         </h1>
-        <b-field>
-          <b-input placeholder="Email" type="email" rounded />
-          <p class="control">
-            <b-button class="button is-info" rounded>
-              Ask The Trainers
-            </b-button>
-          </p>
-        </b-field>
+        <h2 class="subtitle">
+          Put down your email and we'll reach out!
+        </h2>
+        <form
+          name="emails"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="form-name" value="emails" />
+          <b-field>
+            <b-input
+              v-model="email"
+              placeholder="example@example.com"
+              type="email"
+              rounded
+            />
+            <p class="control">
+              <b-button
+                type="submit"
+                class="button is-info"
+                rounded
+                @click="askButtonClick()"
+              >
+                Ask The Trainers
+              </b-button>
+            </p>
+          </b-field>
+        </form>
       </div>
     </div>
   </section>
@@ -21,5 +42,20 @@
 <script lang="ts">
 //The hero banner for the top of the landing page
 import Vue from 'vue';
-export default Vue.extend({});
+export default Vue.extend({
+  data() {
+    return {
+      email: '',
+    };
+  },
+  methods: {
+    askButtonClick() {
+      this.email = '';
+      this.$buefy.snackbar.open({
+        message: "Thank you! We'll reach out!",
+        position: 'is-top',
+      });
+    },
+  },
+});
 </script>
